f42443095ba6494277909ee106dc970cf1051746
gizem <gizemsacal@gmail.com>
2021-05-10 16:26:03 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+class Main {
+
+    public static void main(String[] args) {
+        // Creating ConcurrentMap using ConcurrentHashMap
+        ConcurrentMap<String, Integer> numbers = new ConcurrentHashMap<>();
+
+        // Insert elements to map
+        numbers.put("Two", 2);
+        numbers.put("One", 1);
+        numbers.put("Three", 3);
+        System.out.println("ConcurrentMap: " + numbers);
+
+        // Access the value of specified key
+        int value = numbers.get("One");
+        System.out.println("Accessed Value: " + value);
+
+        // Remove the value of specified key
+        int removedValue = numbers.remove("Two");
+        System.out.println("Removed Value: " + removedValue);
+    }
+}
